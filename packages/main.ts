8744b81eac595f0dfe99a72c31c44fc12635213b
LittleRangiferTarandus <694408762@qq.com>
2025-06-03 00:42:22 +0800

    fix: fix the export in main.ts

--- packages/main.ts
+++ packages/main.ts
@@ -9,6 +9,8 @@ export { capitalize } from './string/capitalize'
 export { titleCase } from './string/titleCase'
 export { htmlEncode } from './string/htmlEncode'
 export { htmlDecode } from './string/htmlDecode'
+export { escapeRegExp } from './string/escapeRegExp'
+export { parseTemplate } from './string/parseTemplate'
 
 export { isString } from './typed/isString'
 export { isBoolean } from './typed/isBoolean'
@@ -21,61 +23,71 @@ export { isBigInt } from './typed/isBigInt'
 export { isPrimitive } from './typed/isPrimitive'
 export { isNumberString } from './typed/isNumberString'
 export { isArray } from './typed/isArray'
-
 export { isFunction } from './typed/isFunction'
 export { isObjectLike } from './typed/isObjectLike'
 export { isClass } from './typed/isClass'
 export { isPseudoArray } from './typed/isPseudoArray'
-
 export { isInfinity } from './typed/isInfinity'
 export { isNanValue } from './typed/isNanValue'
 export { isInt } from './typed/isInt'
 export { isFloat } from './typed/isFloat'
+export { getTypeTag } from './typed/getTypeTag'
 
 export { getByPath } from './object/getByPath'
 export { setByPath } from './object/setByPath'
 export { deleteByPath } from './object/deleteByPath'
+export { clone } from './object/clone'
+export { cloneDeep } from './object/cloneDeep'
+export type { CustomizeClone } from './object/cloneDeep'
+export { omit } from './object/omit'
+export { pick } from './object/pick'
+export { filterFields } from './object/filterFields'
+export { forEachFields } from './object/forEachFields'
+export { isEqual } from './object/isEqual'
+export { isEqualStrict } from './object/isEqualStrict'
+export { unzipToArrays } from './object/unzipToArrays'
+export { objectToPairs } from './object/objectToPairs'
+export { splitToArrays } from './object/splitToArrays'
 
 export { difference } from './array/difference'
 export { intersection } from './array/intersection'
 export { union } from './array/union'
 export { symmetricDifference } from './array/symmetricDifference'
 export { unique } from './array/unique'
-
 export { leftJoin } from './array/leftJoin'
 export { sortIndex } from './array/sortIndex'
-
-export { range } from './number/range'
-
 export { joinToObject } from './array/joinToObject'
 export { pairsToObject } from './array/pairsToObject'
 export { zipToObject } from './array/zipToObject'
-export { unzipToArrays } from './object/unzipToArrays'
-export { objectToPairs } from './object/objectToPairs'
-export { splitToArrays } from './object/splitToArrays'
+export { sortWithIndex } from './array/sortWithIndex'
+export { lexSort } from './array/lexSort'
+export { linkToTree } from './array/linkToTree'
+export { chunk } from './array/chunk'
+export { numberSort } from './array/numberSort'
+export { orderSort } from './array/orderSort'
 
-export { clone } from './object/clone'
-export { cloneDeep } from './object/cloneDeep'
-export type { CustomizeClone } from './object/cloneDeep'
-
-export { getTypeTag } from './typed/getTypeTag'
-export { omit } from './object/omit'
-export { pick } from './object/pick'
-export { filterFields } from './object/filterFields'
-export { forEachFields } from './object/forEachFields'
-
-export { isEqual } from './object/isEqual'
-export { isEqualStrict } from './object/isEqualStrict'
+export { range } from './number/range'
+export { percent } from './number/percent'
+export { thousandSeparator } from './number/thousandSeparator'
+export { convertDataUnit } from './number/convertDataUnit'
 
 export { average } from './statistic/average'
 export { count } from './statistic/count'
 export { median } from './statistic/median'
 export { mode } from './statistic/mode'
 export { sum } from './statistic/sum'
+export { max } from './statistic/max'
+export { maxItem } from './statistic/maxItem'
+export { min } from './statistic/min'
+export { minItem } from './statistic/minItem'
+export { modeItem } from './statistic/modeItem'
 
 export { randomBoolean } from './random/randomBoolean'
 export { randomFromArray } from './random/randomFromArray'
 export { randomNumber } from './random/randomNumber'
+export { randomString } from './random/randomString'
+export type { RandomStringOptions } from './random/randomString'
+export { randomInt } from './random/randomInt'
 
 export { combine } from './function/combine'
 export { curry } from './function/curry'
@@ -83,33 +95,16 @@ export { debounce } from './function/debounce'
 export { delay } from './function/delay'
 export { memoize } from './function/memoize'
 export { throttle } from './function/throttle'
+export { once } from './function/once'
+export { withFallback } from './function/withFallback'
 
 export { asyncForEach } from './async/asyncForEach'
 export { asyncForEachFields } from './async/asyncForEachFields'
 export { asyncMap } from './async/asyncMap'
-
 export { concurrent } from './async/concurrent'
 export { sequential } from './async/sequential'
-
 export { retry } from './async/retry'
 export type { RetryOptions } from './async/retry'
-
-export { randomString } from './random/randomString'
-export type { RandomStringOptions } from './random/randomString'
-
-export { sortWithIndex } from './array/sortWithIndex'
-export { lexSort } from './array/lexSort'
-
-export { once } from './function/once'
-export { withFallback } from './function/withFallback'
-
-export { linkToTree } from './array/linkToTree'
-export { chunk } from './array/chunk'
-
-export { escapeRegExp } from './string/escapeRegExp'
-export { parseTemplate } from './string/parseTemplate'
-
-export { convertDataUnit } from './number/convertDataUnit'
 
 export type {
   PrimitiveType,
@@ -127,7 +122,8 @@ export type {
   LiteralStringWithFallback,
   MappedTypeByKeyOrIndex,
   DeepMappedTypeByKeyOrIndex,
-  DataUnit
+  DataUnit,
+  PseudoArray
 } from './common/types'
 
 export {
